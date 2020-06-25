add05a8cee6c08685469db2583a650335fff2579
Hauke Broer <hauke.broer@ise.de>
2020-06-25 17:51:52 +0200

    fixed problem by adding a static displayName

--- dependency/src/components/index.tsx
+++ dependency/src/components/index.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 export class Swipe extends Component<Props> {
+    static displayName = "Swipe";
     render(props: Props,) {
         return <div style={{pointerEvents: "none"}}>{props.children}</div>;
     }
